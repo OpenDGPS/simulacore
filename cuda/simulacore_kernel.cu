790784c2eac2967607d317aba1ce647ba5c75fa1
Rene Meissner <info@opendgps.de>
2018-07-26 14:14:49 +0200

    Every core reads its own executable memory
    Each memory block for every GPU contain its own executable binary.
    Switch between two different executable formats (MachO and Linux Elf).

--- cuda/simulacore_kernel.cu
+++ cuda/simulacore_kernel.cu
@@ -5,9 +5,8 @@
 #include <helper_functions.h>
 #include <helper_cuda.h>
 
-#define BINARYSIZE 8368
-#define NUMOFCORES 384
 
+// 0x30E750
 __device__ int getGlobalIdx_1D_1D()
 {
 	return blockIdx.x *blockDim.x + threadIdx.x;
@@ -17,16 +16,33 @@ __device__ unsigned char getNextByte(int *targetMem, int longInt) {
 	return ((targetMem[longInt >> 2] & (0xff << ( 8 * (longInt % 4) ))) >> ( 8 * (longInt % 4) ));
 }
 
-__global__ void simulacore_gpu(int *targetMem, int *resultMem) {
+__global__ void simulacore_gpu(int *arch, int *targetMem, int *resultMem) {
 	int coreNum = 0;
+	int executableOffset = 0;
 	int opcAddress = 0;
 	int varAddress = 0;
 	unsigned char command;
 	// fake register
-	long rbp_4, rbp_8, eax, ecx; // rsp not needed
+	long rbp_4, rbp_8, eax, ecx, edx; // rsp not needed
 	coreNum = getGlobalIdx_1D_1D();
 	if (coreNum < NUMOFCORES) {
-		opcAddress = 0xf80;
+		// opcAddress = 0xf80; // MachO
+		if (arch[coreNum] == 0x01 ) {
+			if (arch[coreNum] != arch[0] ) {
+				executableOffset = (NUM_OF_MACHO_CORES * BINARYSIZE_LIELF) + ((coreNum - NUM_OF_MACHO_CORES) * BINARYSIZE_LIELF);
+			} else {
+				executableOffset = (coreNum * BINARYSIZE_LIELF);
+			}
+			opcAddress = executableOffset + 0xf80;
+		}
+		if (arch[coreNum] == 0x02 ) {
+			if (arch[coreNum] != arch[0] ) {
+				executableOffset = (NUM_OF_MACHO_CORES * BINARYSIZE_LIELF) + ((coreNum - NUM_OF_MACHO_CORES) * BINARYSIZE_LIELF);
+			} else {
+				executableOffset = (coreNum * BINARYSIZE_LIELF);
+			}
+			opcAddress = executableOffset + 0x4d6;
+		}
 		for ( int step = 0; step < 16; step++ ) {
 			command = getNextByte(targetMem,opcAddress); opcAddress++;
 			if ( command == 0x55 ) {
@@ -59,36 +75,59 @@ __global__ void simulacore_gpu(int *targetMem, int *resultMem) {
 			if ( command == 0x8B ) {	
 				command = getNextByte(targetMem,opcAddress); opcAddress++;
 				if ( command == 0x05 ) {	
-					// mov        eax, dword [_a]	# 8B 05 68 00 00 00  
-					varAddress = 0x1000;
+					// mov        eax, dword [_a]	# 8B 05 68 00 00 00 
+					if (arch[coreNum] == 0x01 ) 
+						varAddress = executableOffset + 0x1000; // MachO
+					if (arch[coreNum] == 0x02 ) 
+						varAddress = executableOffset + 0x1030; // Linux ELF
 					eax = (getNextByte(targetMem,varAddress)); varAddress++;
 					eax = eax + (getNextByte(targetMem,varAddress) << 8); varAddress++;
 					eax = eax + (getNextByte(targetMem,varAddress) << 16); varAddress++;
 					eax = eax + (getNextByte(targetMem,varAddress) << 24); 
+					// resultMem[coreNum] = eax;
 					opcAddress = opcAddress + 4;
 				}
 				if ( command == 0x0d ) {
 					// mov        ecx, dword [_a] # 8B 0D 5B 00 00 00 
-					varAddress = 0x1000;
+					if (arch[coreNum] == 0x01 ) 
+						varAddress = executableOffset + 0x1000; // MachO
+					if (arch[coreNum] == 0x02 ) 
+						varAddress = executableOffset + 0x1030; // Linux ELF
 					ecx = (getNextByte(targetMem,varAddress)); varAddress++;
 					ecx = ecx + (getNextByte(targetMem,varAddress) << 8); varAddress++;
 					ecx = ecx + (getNextByte(targetMem,varAddress) << 16); varAddress++;
 					ecx = ecx + (getNextByte(targetMem,varAddress) << 24); 
 					opcAddress = opcAddress + 4;
 				}
+				if ( command == 0x55 ) {
+					//  mov        edx, dword [rbp+var_4] # 8B 55 FC 
+					edx = rbp_4;
+					opcAddress++; 
+				}
+				if ( command == 0x45 ) {
+					//  mov        eax, dword [rbp+var_4] # 8B 45 FC 
+					eax = rbp_4;
+					opcAddress++; 
+				}
 			}
 			if ( command == 0x0f ) {	
-				command = getNextByte(targetMem,opcAddress); opcAddress++;
+				opcAddress++;
 				command = getNextByte(targetMem,opcAddress); opcAddress++;
 				if ( command == 0x45 ) {
 					// imul       eax, dword [rbp+var_8]	# 0F AF 45 F8 
-					eax = eax * rbp_8;
 					command = getNextByte(targetMem,opcAddress); opcAddress++;
+					if ( command == 0xf8 ) // OSX RBP address downwards from ff 
+						eax = eax * rbp_8;
+					if ( command == 0xfc ) // Linux ELF
+						eax = eax * rbp_4;
 				}
 				if ( command == 0xc1 ) {
 					// imul       eax, ecx	# 0F AF C1  
 					eax = eax * ecx;
-					command = getNextByte(targetMem,opcAddress); opcAddress++;
+				}
+				if ( command == 0xd0 ) {
+					// imul       edx, eax	# 0F AF D0  
+					edx = edx * eax;
 				}
 			}
 			if ( command == 0x03 ) {
@@ -102,6 +141,22 @@ __global__ void simulacore_gpu(int *targetMem, int *resultMem) {
 					// add        ecx, dword [rbp+var_8]	# 03 4D F8  
 					ecx = ecx + rbp_8;
 					command = getNextByte(targetMem,opcAddress); opcAddress++;
+				}
+			}
+			if ( command == 0x83 ) {
+				// add        eax, 0x1 # 83 C0 01 
+				eax = eax + 0x01; // Linux gcc -O0 ???
+				opcAddress++; opcAddress++;
+			}
+			if ( command == 0x01 ) {
+				command = getNextByte(targetMem,opcAddress); opcAddress++;
+				if ( command == 0xd0 ) {
+					// add        eax, edx # 01 D0 
+					eax = eax + edx;
+				}
+				if ( command == 0xca ) {
+					// add        edx, ecx # 01 CA 
+					edx = edx + ecx; 
 				}
 			}
 			resultMem[coreNum] = eax;
